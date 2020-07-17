ed5d80058083fd5acfd7c336ffa4446b2bf228e8
Desmond Rogers <desmondrogers2@gmail.com>
2020-07-17 15:43:50 -0500

    Reconfigure linter-phpcs

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH=/usr/local/mysql/bin:/usr/local/bin:"$HOME/Dropbox/notes/todo/":$PATH
+export PATH=/usr/local/mysql/bin:"$HOME/Dropbox/notes/todo/":$PATH
+export PATH="$PATH:$HOME/.composer/vendor/bin/phpcs"
 export PATH=$(pyenv root)/shims:$PATH
