ad8fdee319966b38cc7d5d2b32da3e06a0770957
Desmond Rogers <desmondrogers2@gmail.com>
2020-07-18 19:30:39 -0500

    Add git log to aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,3 +8,4 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
