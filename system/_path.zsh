cdffbd74eb9dccc52abfb43cf0db0622c674846b
Desmond Rogers <32533354+desrogers@users.noreply.github.com>
2024-05-23 17:32:03 -0500

    update pyenv

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,2 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH=/usr/local/mysql/bin:"$HOME/Dropbox/notes/todo/":$PATH
-export PATH="$PATH:$HOME/.composer/vendor/bin/phpcs"
-export PATH=$(pyenv root)/shims:$PATH
