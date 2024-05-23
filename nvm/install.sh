1ea3df3cab181a3136023cd6fd5233763768910d
Desmond Rogers <32533354+desrogers@users.noreply.github.com>
2024-05-23 16:44:27 -0500

    update nvm install script version

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 if ![[ $HOME/.nvm ]] 1>/dev/null 2>&1; then
-  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    command -v nvm
 fi
+
