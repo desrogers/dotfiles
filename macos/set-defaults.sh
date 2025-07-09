d301114a8516aaf92a78eaf70b8abc7b79e7c4a5
Desmond Rogers <32533354+desrogers@users.noreply.github.com>
2025-07-09 14:03:21 -0500

    change macos defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,7 +1,5 @@
 # Sets reasonable macOS defaults.
 #
-# Or, in other words, set shit how I like in macOS.
-#
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
@@ -14,7 +12,7 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 chflags nohidden ~/Library
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
 # Show the full path in the Finder's title
