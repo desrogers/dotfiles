4337a824784783bcefd872a46761d3fa81ac61b2
Desmond Rogers <32533354+desrogers@users.noreply.github.com>
2025-07-09 18:55:01 -0500

    fix git bash completion

--- git/completion.bash
+++ git/completion.bash
@@ -1,8 +1,8 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below 
-completion='$(brew --prefix)/etc/bash_completion.d/git-completion.bash'
+completion="$(brew --prefix)/etc/bash_completion.d/git-completion.bash"
 
-if test -f $completion
+if [ -f $completion ]
 then
   source $completion
 fi
